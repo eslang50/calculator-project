8d196766fa47dade9916e68f9ece602015ca9c21
eslang50 <ethanliu50@gmail.com>
2022-08-22 00:16:38 -0400

    addedoperation functionality and styling

--- calc.js
+++ calc.js
@@ -12,14 +12,65 @@ function operate(operator, a, b) {
   return operator(a,b);
 }
 
-// Display digit function
+let firstInput = 0;
+let secondInput = 0;
+let operatorClicked = false;
+let operator;
 
 function display(input) {
+
   if(input === 'C') {
     document.getElementById("input").value = '';
+    firstInput = 0;
+    secondInput = 0;
+    operatorClicked = false;
   }
+
+  else if(input === "+" || input === "-" || input === "/" || input === "*") {
+    operatorClicked = true;
+    if(input === "+") {
+      operator = add;
+      document.getElementById("input").value += input;
+      secondInput = 0;
+    }
+    else if(input === "-") {
+      operator = subtract;
+      document.getElementById("input").value += input;
+      secondInput = 0;
+    }
+    else if(input === "*") {
+      operator = multiply;
+      document.getElementById("input").value += input;
+      secondInput = 0;
+    }
+    else {
+      operator = divide;
+      document.getElementById("input").value += input;
+      secondInput = 0;
+    }
+  }
+
+  else if(input === "=") {
+    let result = operate(operator, firstInput, secondInput);
+    document.getElementById("input").value = result;
+    firstInput = result;
+    secondInput = 0;
+  }
+
   else {
+
     document.getElementById("input").value += input;
+
+    if(operatorClicked == false) {
+      firstInput = 10*firstInput + input;
+    }
+
+    else if(operatorClicked == true) {
+      secondInput = 10*secondInput + input;
+      
+    }
+
   }
-  
+ 
 }
+
